473afece754cdea6c363074c70cb8fb2570b5e5e
Bartosz Jakubiec <jakubiecwiksa@gmail.com>
2020-04-14 21:51:30 +0200

    podstawowy setup aplikacji

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './scss/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ksbdkjs
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          skdskl;d
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
