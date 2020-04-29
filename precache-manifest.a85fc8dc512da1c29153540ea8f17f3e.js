3c1580cac32122acf46444eae873c9bdce3216d8
Bartosz Jakubiec <jakubiecwiksa@gmail.com>
2020-04-29 15:32:06 +0200

    Updates

---
+++ precache-manifest.a85fc8dc512da1c29153540ea8f17f3e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d6b1c30d82920c15d2ca1bd8492121",
+    "url": "/bjportfolio/index.html"
+  },
+  {
+    "revision": "175977b872b7ce5e91e5",
+    "url": "/bjportfolio/static/css/main.1008b628.chunk.css"
+  },
+  {
+    "revision": "ee1857b297aa779299d4",
+    "url": "/bjportfolio/static/js/2.55c3f88f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bjportfolio/static/js/2.55c3f88f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175977b872b7ce5e91e5",
+    "url": "/bjportfolio/static/js/main.cf88abd8.chunk.js"
+  },
+  {
+    "revision": "3203d131b57993354574",
+    "url": "/bjportfolio/static/js/runtime-main.79edbce5.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/bjportfolio/static/media/3ColumnsBackground.99f2e620.png"
+  },
+  {
+    "revision": "28770ab2f4bfac963e4bd94740f238be",
+    "url": "/bjportfolio/static/media/BackgroundImage.28770ab2.png"
+  },
+  {
+    "revision": "6baf5950179f12f109e487a7ec03faac",
+    "url": "/bjportfolio/static/media/Decoration.6baf5950.svg"
+  },
+  {
+    "revision": "76b47d5049797ef506d5a242168a14a4",
+    "url": "/bjportfolio/static/media/HeroImage.76b47d50.png"
+  },
+  {
+    "revision": "80a70b6f49e0aa104ea9400badd00572",
+    "url": "/bjportfolio/static/media/People.80a70b6f.png"
+  },
+  {
+    "revision": "d87cadf24b263287f4d33024c61c02e5",
+    "url": "/bjportfolio/static/media/Signature.d87cadf2.svg"
+  }
+]);
